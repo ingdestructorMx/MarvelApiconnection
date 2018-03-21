1a5ec0d461039967ffa5b2ba1c9cb4ccdc051537
unknown <griegomecatronica@gmail.com>
2018-03-20 23:50:24 -0700

    20/03/2018

--- app/src/main/java/com/example/oscar/marvelheroes/MainActivity.java
+++ app/src/main/java/com/example/oscar/marvelheroes/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.oscar.marvelheroes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
